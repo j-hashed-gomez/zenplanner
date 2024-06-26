d4ab557b4440cfb3a3d8d49a3d70848d3ee813bf
Jose Luis <joseluis.gomez@ingrammicro.com>
2024-06-26 17:55:58 +0200

    Adding Session Control

--- settings.py
+++ settings.py
@@ -113,6 +113,7 @@ DEFAULT_CHARSET = 'utf-8'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
