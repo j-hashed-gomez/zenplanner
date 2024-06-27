bd9b9f18c2e0f156c385c9d669f7d4fd5e76ef1b
Jose Luis <joseluis.gomez@ingrammicro.com>
2024-06-27 16:34:28 +0200

    Adding Google Auth and DB

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*').split(',')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
+        'NAME': os.getenv('DB_NAME', 'zenplanner'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
