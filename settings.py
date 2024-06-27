9ee5440d5eaf8542499a5bdbd9db06dd5f560366
Jose Luis <joseluis.gomez@ingrammicro.com>
2024-06-27 08:51:17 +0200

    Adding Session Control

--- settings.py
+++ settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'zenplanner.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'zenplanner/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -59,6 +59,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'zenplanner.wsgi.application'
 
