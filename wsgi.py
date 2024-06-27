b5c104eea215f919fe9444d935c8a85c4d01dc06
Jose Luis <joseluis.gomez@ingrammicro.com>
2024-06-27 17:31:08 +0200

    Adding Google Auth and DB

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,13 @@
+"""
+WSGI config for zenplanner project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
 import os
-import sys
-
-# Ruta al entorno virtual
-venv_path = '/app/venv'
-
-# Añadir la ruta del entorno virtual al sys.path
-sys.path.append(venv_path + '/lib/python3.9/site-packages')
-
-# Añadir la ruta de tu proyecto al sys.path
-sys.path.append('/app')
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zenplanner.settings')
