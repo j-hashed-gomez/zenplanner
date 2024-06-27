33014ee87fc3386c28d118f8111e3b8d4b4d1019
Jose Luis <joseluis.gomez@ingrammicro.com>
2024-06-27 15:52:36 +0200

    Adding Google Auth and DB

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,18 @@ set -e
 
 # Ejecutar las tareas de inicialización
 echo "Running database migrations..."
-/app/venv/bin/python /app/manage.py migrate
+if /app/venv/bin/python /app/manage.py migrate; then
+  echo "Database migrations completed successfully."
+else
+  echo "Database migrations failed. Continuing with the startup..."
+fi
 
 echo "Collecting static files..."
-/app/venv/bin/python /app/manage.py collectstatic --noinput
+if /app/venv/bin/python /app/manage.py collectstatic --noinput; then
+  echo "Static files collected successfully."
+else
+  echo "Collecting static files failed. Continuing with the startup..."
+fi
 
 # Ejecutar el servidor Apache en primer plano
 exec "$@"
