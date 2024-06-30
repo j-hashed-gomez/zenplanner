872e0aeb94d654205fbc6362bdf30633340fb0e4
Jose Luis <joseluis.gomez@ingrammicro.com>
2024-06-30 12:06:30 +0200

    Adding calendars

--- urls.py
+++ urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('google-login/', google_login, name='google_login'),
     path('callback/', google_callback, name='google_callback'),
     path('logout/', logout_view, name='logout'),
-    path('api/get-reserved-slots/', get_reserved_slots),  # Asegúrate de que esta línea tenga el paréntesis de cierre
+    path('api/get-reserved-slots/', get_reserved_slots), 
+    path('api/get-reserved-slots/', get_reserved_slots, name='get_reserved_slots'),
 ]
